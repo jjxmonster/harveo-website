4fbf5299c144260226044601513a369977e4a5dc
jjxmonster <kuba.tarabasz@wp.pl>
2021-06-18 10:19:59 +0200

    loading indicator

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as LandingPage } from './LandingPage';
 export { default as NavBar } from './NavBar';
 export { default as WelcomePage } from './WelcomePage';
+export { default as LoadingIndicator } from './LoadingIndicator';
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React, { Suspense } from "react";
-import ReactDOM from "react-dom";
-import "assets/scss/material-kit-react.scss?v=1.10.0";
-import App from "App.js";
-import './i18next/i18n.js'
-
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import 'assets/scss/material-kit-react.scss?v=1.10.0';
+import App from 'App.js';
+import './i18next/i18n.js';
+import { LoadingIndicator } from 'components';
 
 ReactDOM.render(
-    <Suspense fallback="loading">
-      <App/>
-    </Suspense>,
-  document.getElementById("root")
+   <Suspense fallback={<LoadingIndicator />}>
+      <App />
+   </Suspense>,
+   document.getElementById('root')
 );
